38134a1ecc6768e24faa21619a99d25107bebcab
zBiggui <bigguisenna@gmail.com>
2021-08-30 23:57:43 -0300

    Novas rotas

--- index.mjs
+++ index.mjs
@@ -16,6 +16,18 @@ app.get('/clientes', (req, res) => {
     res.send(['Mathias', 'José', 'Thiago'])
 });
 
+app.get('/pag1', (req, res) => {
+    res.send('Página 1')
+});
+
+app.get('/pag2', (req, res) => {
+    res.send('Página 2')
+});
+
+app.get('/pag3', (req, res) => {
+    res.send('Página 3')
+});
+
 app.post('/', (req, res) => {
     res.send('Hello Post!')
 });
